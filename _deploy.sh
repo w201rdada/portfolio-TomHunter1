7460c978d64583633cd9e2c44a73ae9995868178
w201rdada-portfolio-tomhunter1-av5me7zt <ethunter90@gmail.com>
2018-02-07 04:07:18 +0000

    deploy

--- _deploy.sh
+++ _deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # put your info here and uncomment so git (and GitHub) knows who your commits belong to!
-# git config user.email "calnet@berkeley.edu"
-# git config user.name "Oski Bear"
+git config --global user.email "ethunter90@gmail.com"
+git config user.name "TomHunter1"
 
 # make sure you're on the master branch
 git checkout master
